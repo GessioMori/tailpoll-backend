1e301b771bedbdbf5230ff74d1652a2837318764
Gessio Mori <gessiomorin@gmail.com>
2022-11-11 10:15:18 -0300

    implements rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { PoolController } from './controllers/pool.controller';
 import { VoteController } from './controllers/vote.controller';
 import { PoolService } from './services/pool.service';
@@ -7,8 +9,22 @@ import { PrismaService } from './services/prisma.service';
 import { VoteService } from './services/vote.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 30,
+      limit: 10,
+    }),
+  ],
   controllers: [PoolController, VoteController],
-  providers: [PoolService, VoteService, PrismaService],
+  providers: [
+    PoolService,
+    VoteService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
